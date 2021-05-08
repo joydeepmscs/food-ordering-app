00debb6b729e87b6bce6164edae2af3cdf86addf
joydeepmscs <joydeep.mscs@gmail.com>
2021-05-08 14:50:56 +0100

    Address and Customer API code refactoring

--- food-ordering-app-service/src/main/java/com/upgrad/foodorderingapp/service/dao/StateDao.java
+++ food-ordering-app-service/src/main/java/com/upgrad/foodorderingapp/service/dao/StateDao.java
@@ -1,18 +1,18 @@
 package com.upgrad.foodorderingapp.service.dao;
 
 import com.upgrad.foodorderingapp.service.entity.StateEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class StateDao {
-
-
+    private final Logger log = LoggerFactory.getLogger(StateDao.class);
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -22,28 +22,24 @@ public class StateDao {
      * @param stateUuid
      * @return
      */
-    public StateEntity getStateByUUID(String stateUuid) {
+    public StateEntity getStateByUUID(final String stateUuid) {
         try {
             return entityManager.createNamedQuery("stateByUUID", StateEntity.class)
                     .setParameter("stateUuid", stateUuid)
                     .getSingleResult();
         } catch (NoResultException nre) {
+            log.info("State with uuid doesn't exist in db: {}", stateUuid);
             return null;
         }
     }
 
     /**
-     * Method to retrieve all statename from the database
+     * Method to retrieve all states from the database
      *
      * @param
      * @return
      */
     public List<StateEntity> getAllStates() {
-        try {
-            List<StateEntity> stateEntities = entityManager.createNamedQuery("getAllStates", StateEntity.class).getResultList();
-            return stateEntities;
-        } catch (NoResultException nre) {
-            return Collections.emptyList();
-        }
+        return entityManager.createNamedQuery("getAllStates", StateEntity.class).getResultList();
     }
 }
