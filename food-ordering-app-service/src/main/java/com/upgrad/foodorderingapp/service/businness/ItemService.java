97347c4b107a3f398c183cdab65faef91f24a770
joydeepmscs <joydeep.mscs@gmail.com>
2021-05-10 13:36:31 +0100

    changes to return any 5 items when no order is being placed from the restaurant

--- food-ordering-app-service/src/main/java/com/upgrad/foodorderingapp/service/businness/ItemService.java
+++ food-ordering-app-service/src/main/java/com/upgrad/foodorderingapp/service/businness/ItemService.java
@@ -81,6 +81,13 @@ public class ItemService {
             }
 
         }
+        for (RestaurantItemEntity rest:restaurantItemEntities){
+            if(!finalList.contains(rest) && counter<=5){
+                finalList.add(rest);
+                counter++;
+            }
+
+        }
 
         return finalList;
     }
